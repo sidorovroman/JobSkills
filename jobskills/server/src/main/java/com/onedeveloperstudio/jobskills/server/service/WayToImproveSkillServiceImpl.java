765220f1785ed1be2ec60608bf31445b788fe6c7
cooldman1 <optimist91@bk.ru>
2014-09-20 14:41:24 +0400

    Правка контроллеров

---
+++ jobskills/server/src/main/java/com/onedeveloperstudio/jobskills/server/service/WayToImproveSkillServiceImpl.java
@@ -0,0 +1,15 @@
+package com.onedeveloperstudio.jobskills.server.service;
+
+import com.onedeveloperstudio.core.server.service.BaseService;
+import com.onedeveloperstudio.core.server.service.BaseServiceImpl;
+import com.onedeveloperstudio.jobskills.common.dto.WayToImproveSkillDto;
+import org.springframework.stereotype.Service;
+
+/**
+ * User: User
+ * Date: 20.09.14
+ */
+
+@Service
+public class WayToImproveSkillServiceImpl extends BaseServiceImpl<WayToImproveSkillDto> implements WayToImproveSkillService {
+}
