344e50342306216c88d481f54e208993d3db282b
Роман петрович <sidorovroman@some-2.local>
2015-02-19 00:11:49 +0400

    мы умеем логиниться, выходить и регаться. при успешной регистрации нас перекидывает на главную страницу, но чтобы оказаться авторизованным надо обновить страницу

--- serverApp/src/main/webapp/resources/js/controllers/login.js
+++ serverApp/src/main/webapp/resources/js/controllers/login.js
@@ -3,6 +3,7 @@
 
     app.controller("LoginCtrl", function ($scope, $location, $http, $routeParams) {
         $scope.LoginForm = {};
+
         $scope.reg = function () {
             console.log("reg");
 
@@ -11,13 +12,15 @@
                 password: this.LoginForm.password
             };
 
-            $http.post('/register',dataObject).
-                success(function (data) {
-                    console.log("успех");
-                }).
-                error(function (data) {
-                    console.log("Fail");
+            $http.post('/register',dataObject)
+                .success(function(response){
+                    if(response.error==null){
+                        console.log("reg success");
+                        $location.path('/');
+                    }else{
+                        alert(response.error);
+                    }
                 });
-        }
+        };
     });
 })();
