a79c421b22e047f2f7a09de3f61c359daa0ce9fd
Роман петрович <sidorovroman@some-2.local>
2015-03-14 22:41:27 +0400

    поправил метод регистрации

--- serverApp/src/main/webapp/resources/js/controllers/loginModal.js
+++ serverApp/src/main/webapp/resources/js/controllers/loginModal.js
@@ -2,12 +2,13 @@
     var app = angular.module('loginModal', []);
 
     app.controller('ModalLoginCtrl', function ($scope, $http, $location) {
-
-        var dataObject = {
-            email: this.mail,
-            password: this.password
-        };
         $scope.login = function () {
+
+            var dataObject = {
+                mail: $scope.mail,
+                password: $scope.password
+            };
+
             $http.post('/j_spring_security_check',dataObject)
                 .success(function(response){
                     if(response.error==null){
@@ -18,15 +19,22 @@
                 });
         };
         $scope.register = function () {
-            $http.post('/register',dataObject)
-                .success(function(response){
-                    if(response.error==null){
+            $http.post('/register',
+                {
+                    mail:$scope.mail,
+                    password: $scope.password
+                }).
+                success(function(data, status, headers, config) {
+                    console.log("post success");
+                    if(data.error==null){
                         console.log("register success");
                     }else{
-                        alert(response.error);
+                        alert(data.error);
                     }
+                }).
+                error(function(data, status, headers, config) {
+                    console.log("post error");
                 });
-
         };
     })
 
