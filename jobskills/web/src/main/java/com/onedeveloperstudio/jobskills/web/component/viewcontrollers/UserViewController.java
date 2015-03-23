65fb7a9b25e63e84a1ae5b441c84e3b45c96f10b
y.zakharov <y.zakharov@iis-soft.ru>
2015-03-23 13:16:55 +0300

    Возможность обновления информации о пользователе

--- jobskills/web/src/main/java/com/onedeveloperstudio/jobskills/web/component/viewcontrollers/UserViewController.java
+++ jobskills/web/src/main/java/com/onedeveloperstudio/jobskills/web/component/viewcontrollers/UserViewController.java
@@ -3,19 +3,17 @@ package com.onedeveloperstudio.jobskills.web.component.viewcontrollers;
 import com.onedeveloperstudio.core.common.dto.SysUserDto;
 import com.onedeveloperstudio.core.common.dto.User;
 import com.onedeveloperstudio.core.server.service.SysUserService;
-import flexjson.JSONSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * User: y.zakharov
@@ -35,5 +33,31 @@ public class UserViewController {
     User regUser = (User) auth.getPrincipal();
     SysUserDto user = service.loadByEmail(regUser.getUsername());
     return user;
+  }
+
+  @RequestMapping("/update")
+  @ResponseBody
+  public SysUserDto updateUserInfo(@RequestBody SysUserDto updatedInfo){
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+    User regUser = (User) auth.getPrincipal();
+    SysUserDto user = service.loadByEmail(regUser.getUsername());
+    if(user.getId().equals(updatedInfo.getId())){
+      user.setCountry(updatedInfo.getCountry());
+      user.setPhone(updatedInfo.getPhone());
+      user.setUserFullName(updatedInfo.getUserFullName());
+      user.setBirthday(updatedInfo.getBirthday());
+      user.setCity(updatedInfo.getCity());
+      user.setSex(updatedInfo.getSex());
+      return service.update(user);
+    } else {
+      throw new AccessDeniedException("Запрещено обновлять информацию другого пользователя");
+    }
+  }
+
+  @ResponseBody
+  @ExceptionHandler(Exception.class)
+  public String handleAllException(Exception ex) {
+    ex.printStackTrace();
+    return "{\"error\":\"" + ex.getLocalizedMessage() + "\"}";
   }
 }
